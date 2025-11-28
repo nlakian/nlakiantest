3ec29c103588247e3f0d0cf10271e402cb835bf0
Nurina Lakian <nalakian01202500382@usep.edu.ph>
2025-11-28 06:38:08 +0000

    My first push

---
+++ LakianTest.java
@@ -0,0 +1,8 @@
+public class LakianTest {
+    public static void main (String [] args){
+        int x = 0;
+        System.out.printf("Why is zero %d?", x);
+        System.out.printf("\nWhy is zero %d?", x);
+        int y=0;
+    }
+}
